37c7307c35aa38b8398db768a779d622c50d5a1b
Anas <ANas12091101@outlook.com>
2023-04-15 07:33:00 -0400

    Pushing the first time

---
+++ script.js
@@ -0,0 +1,71 @@
+const projects = [
+    { name: "Console Based Shopping Application", technology: "C" },
+    { name: "Window forms based Shopping Application", technology: "C# Windows Forms" },
+    { name: "Artificial Neural Network with linked lists", technology: "C++" },
+    { name: "Linux System call handling the dining philosophers problem", technology: "Linux + C" },
+    { name: "Decoration Ecommerce Website", technology: "Python Django" },
+    { name: "Product Title Classification", technology: "Python Keras and tensorflow / AI" },
+    { name: "Image Forgery Detection", technology: "Python OpenCV" },
+    { name: "Multilabel Image Classification on ImageNet 2013 dataset", technology: "Python Keras and Tensorflow" },
+
+];
+
+const courses = [
+    "Maths",
+    "Physics",
+    "Urdu",
+    "English",
+    "Sindhi",
+    "Computer",
+    "Calculus",
+    "Programming Fundamentals",
+    "OOP",
+    "DS",
+    "OS",
+    "DB",
+    "CN",
+    "Information Security",
+    "DLP",
+
+];
+
+
+function filterProjects() {
+    let filterInput = document.getElementById("filterInput");
+    let projectsList = document.getElementById("projectsList");
+    let filter = filterInput.value.toUpperCase();
+    let filteredProjects = projects.filter(project => project.technology.toUpperCase().includes(filter));
+
+    projectsList.innerHTML = "";
+    filteredProjects.forEach(project => {
+        let projectDiv = document.createElement("div");
+        projectDiv.className = "project";
+        projectDiv.textContent = project.name + " (" + project.technology + ")";
+        projectsList.appendChild(projectDiv);
+    });
+}
+
+
+function populateCourses() {
+    console.log("Here")
+    let coursesList = document.getElementById("coursesList");
+
+    courses.forEach(course => {
+        let courseLi = document.createElement("li");
+        courseLi.className = "course";
+        courseLi.textContent = course;
+        coursesList.appendChild(courseLi);
+    });
+}
+
+function showProjects(){
+    let projectsList = document.getElementById("projectsList");
+    console.log(projectsList)
+    projects.forEach(project => {
+        let projectDiv = document.createElement("div");
+        projectDiv.className = "project";
+        projectDiv.textContent = project.name + " (" + project.technology + ")";
+        projectsList.appendChild(projectDiv);
+    });
+}
+
